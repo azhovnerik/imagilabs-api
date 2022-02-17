5535b7b56fdc43335f1fc49a99fe56b44a47b8e6
aam <aam@anadeainc.com>
2022-02-17 17:11:15 +0200

    Add Kotlin support library for SpringDocs.

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springdoc:springdoc-openapi-kotlin:$springDocVersion")
 
     // Web
     implementation("org.springframework.boot:spring-boot-starter-web")
