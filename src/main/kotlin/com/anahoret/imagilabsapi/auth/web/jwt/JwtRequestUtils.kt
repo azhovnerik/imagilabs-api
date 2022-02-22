097e5ffb5c3ab5f23601c8c900b8c1d04e432c5a
aam <aam@anadeainc.com>
2022-02-22 16:03:45 +0200

    Add URL decoding for authorization header.

--- src/main/kotlin/com/anahoret/imagilabsapi/auth/web/jwt/JwtRequestUtils.kt
+++ src/main/kotlin/com/anahoret/imagilabsapi/auth/web/jwt/JwtRequestUtils.kt
@@ -10,13 +10,14 @@ fun HttpServletRequest.getToken(): String? {
 
 private fun HttpServletRequest.getTokenFromHeader(): String? {
     return getHeader(JwtAuthorizationTokenFilter.JWT_TOKEN_HEADER_NAME)
+        .let { URLDecoder.decode(it, Charsets.UTF_8) }
         ?.takeIf { it.startsWith("${JwtAuthorizationTokenFilter.JWT_TOKEN_HEADER_PREFIX} ") }
         ?.substring(JwtAuthorizationTokenFilter.JWT_TOKEN_HEADER_PREFIX.length + 1)
 }
 
 private fun HttpServletRequest.getTokenFromCookie(): String? {
     return cookies?.find { it.name == JwtAuthorizationTokenFilter.JWT_TOKEN_HEADER_NAME }?.value
-        ?.let { URLDecoder.decode(it, "UTF-8") }
+        ?.let { URLDecoder.decode(it, Charsets.UTF_8) }
         ?.takeIf { it.startsWith("${JwtAuthorizationTokenFilter.JWT_TOKEN_HEADER_PREFIX} ") }
         ?.substring(JwtAuthorizationTokenFilter.JWT_TOKEN_HEADER_PREFIX.length + 1)
 }
