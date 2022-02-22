9a20f9bd04edf413bbf8a3080c07896bac0b2db7
aam <aam@anadeainc.com>
2022-02-22 18:45:23 +0200

    Add student classroom link model.

---
+++ src/main/kotlin/com/anahoret/imagilabsapi/studentclassroomlink/storage/StudentClassroomLink.kt
@@ -0,0 +1,41 @@
+package com.anahoret.imagilabsapi.studentclassroomlink.storage
+
+import org.hibernate.Hibernate
+import java.io.Serializable
+import java.util.*
+import javax.persistence.*
+
+@Entity
+@Table(name = "classrooms_students")
+@IdClass(StudentClassroomLinkId::class)
+class StudentClassroomLink(
+    @Id
+    @Column(name = "student_id", nullable = false)
+    var studentId: UUID,
+
+    @Id
+    @Column(name = "classroom_id", nullable = false)
+    var classroomId: UUID
+)
+
+class StudentClassroomLinkId(
+    var studentId: UUID,
+    var classroomId: UUID
+) : Serializable {
+
+    companion object {
+
+        private const val serialVersionUID = 3887180182994153166L
+    }
+
+    override fun hashCode(): Int = Objects.hash(studentId, classroomId)
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+
+        other as StudentClassroomLinkId
+
+        return studentId == other.studentId &&
+            classroomId == other.classroomId
+    }
+}
--- src/main/resources/db/migration/V4__classrooms.sql
+++ src/main/resources/db/migration/V4__classrooms.sql
@@ -8,12 +8,12 @@ CREATE TABLE classrooms (
     CONSTRAINT pk_classrooms PRIMARY KEY (id)
 );
 
-CREATE TABLE classroom_students(
+CREATE TABLE classrooms_students(
     student_id UUID NOT NULL,
     classroom_id UUID NOT NULL,
-    CONSTRAINT fk_classroom_students_sp FOREIGN KEY (student_id) REFERENCES student_profiles(id),
-    CONSTRAINT fk_classroom_students_c FOREIGN KEY (classroom_id) REFERENCES classrooms(id),
-    CONSTRAINT pk_classroom_students PRIMARY KEY (student_id, classroom_id)
+    CONSTRAINT fk_classrooms_students_sp FOREIGN KEY (student_id) REFERENCES student_profiles(id),
+    CONSTRAINT fk_classrooms_students_c FOREIGN KEY (classroom_id) REFERENCES classrooms(id),
+    CONSTRAINT pk_classrooms_students PRIMARY KEY (student_id, classroom_id)
 );
 
 ALTER TABLE student_profiles
