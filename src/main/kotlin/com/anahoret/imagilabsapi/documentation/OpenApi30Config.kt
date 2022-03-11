ce13362618b6ebe12d6f33743e55f6d25cb0c28d
aam <aam@anadeainc.com>
2022-03-11 12:23:16 +0200

    Add roles information to Swagger API.

--- src/main/kotlin/com/anahoret/imagilabsapi/documentation/OpenApi30Config.kt
+++ src/main/kotlin/com/anahoret/imagilabsapi/documentation/OpenApi30Config.kt
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.security.SecurityScheme
 import org.springdoc.core.PropertyResolverUtils
 import org.springdoc.core.SecurityService
+import org.springdoc.core.customizers.OperationCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.annotation.AnnotationUtils
@@ -24,20 +25,36 @@ class OpenApi30Config {
     fun securityParser(propertyResolverUtils: PropertyResolverUtils): SecurityService {
         return object : SecurityService(propertyResolverUtils) {
             override fun getSecurityRequirementsForMethod(
-                method: Method?,
+                method: Method,
                 allSecurityTags: MutableSet<SecurityRequirement>?
             ): MutableSet<SecurityRequirement> {
                 return (allSecurityTags ?: mutableSetOf())
-                    .apply { getSecuredAnnotation(method)?.let(::add) }
+                    .also { securityRequirements ->
+                        getSecuredAnnotation(method)
+                            ?.let { SecurityRequirement(name = "bearerAuth", scopes = emptyArray()) }
+                            ?.let(securityRequirements::add)
+                    }
                     .let { super.getSecurityRequirementsForMethod(method, it) }
             }
 
-            private fun getSecuredAnnotation(method: Method?): SecurityRequirement? {
-                return method?.let {
-                    AnnotationUtils.findAnnotation(it, Secured::class.java)
-                        ?: AnnotationUtils.findAnnotation(it.declaringClass, Secured::class.java)
-                }?.let { SecurityRequirement(name = "bearerAuth", scopes = emptyArray()) }
-            }
+        }
+    }
+
+    @Bean
+    fun roleOperationCustomizer(): OperationCustomizer {
+        return OperationCustomizer { operation, handlerMethod ->
+            val secured = getSecuredAnnotation(handlerMethod.method)
+            val roles = secured?.value?.toList()?.toString() ?: "[ALL]"
+            val description = operation.description
+            operation.description = "Roles: $roles${description?.let { "<br/><br/>$it" }.orEmpty()}"
+            operation
+        }
+    }
+
+    private fun getSecuredAnnotation(method: Method): Secured? {
+        return method.let {
+            AnnotationUtils.findAnnotation(it, Secured::class.java)
+                ?: AnnotationUtils.findAnnotation(it.declaringClass, Secured::class.java)
         }
     }
 
