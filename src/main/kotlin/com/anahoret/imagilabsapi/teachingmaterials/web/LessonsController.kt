a4b1066cfa3fc856806d0fca8c56ece2f9d24931
aam <aam@anadeainc.com>
2022-03-30 17:05:12 +0300

    Simplify teacher lessons get endpoint.

--- src/main/kotlin/com/anahoret/imagilabsapi/teachingmaterials/web/LessonsController.kt
+++ src/main/kotlin/com/anahoret/imagilabsapi/teachingmaterials/web/LessonsController.kt
@@ -7,7 +7,7 @@ import com.anahoret.imagilabsapi.common.web.SuccessResponseDto
 import com.anahoret.imagilabsapi.common.web.mapErrors
 import com.anahoret.imagilabsapi.security.UserRole
 import com.anahoret.imagilabsapi.teachingmaterials.domain.TeacherLesson
-import com.anahoret.imagilabsapi.teachingmaterials.domain.TeacherLessonsGetUseCase
+import com.anahoret.imagilabsapi.teachingmaterials.domain.TeacherLessonService
 import com.anahoret.imagilabsapi.teachingmaterials.domain.TeacherLessonsUpdateRequest
 import com.anahoret.imagilabsapi.teachingmaterials.domain.TeacherLessonsUpdateUseCase
 import org.springframework.http.ResponseEntity
@@ -18,7 +18,7 @@ import java.util.*
 @RestController
 class LessonsController(
     private val teacherLessonsUpdateUseCase: TeacherLessonsUpdateUseCase,
-    private val teacherLessonsGetUseCase: TeacherLessonsGetUseCase
+    private val teacherLessonService: TeacherLessonService
 ) {
 
     @Secured(UserRole.admin)
@@ -37,8 +37,8 @@ class LessonsController(
     @GetMapping("/api/teachers/{teacherId}/lessons")
     fun getLessonsByTeacher(
         @PathVariable teacherId: UUID
-    ): ResponseEntity<ResponseDto<List<TeacherLesson>>> {
-        return ResponseEntity.ok(SuccessResponseDto(teacherLessonsGetUseCase.get(teacherId)))
+    ): ResponseDto<List<TeacherLesson>> {
+        return SuccessResponseDto(teacherLessonService.listByTeacherId(teacherId))
     }
 
 }
