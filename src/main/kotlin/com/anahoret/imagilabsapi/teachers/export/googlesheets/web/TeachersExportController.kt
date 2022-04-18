41e3695861da3b1dbcdd051d7d7a425a8a8883e0
aam <aam@anadeainc.com>
2022-04-18 18:10:18 +0300

    Add get teachers export google sheet URI endpoint for admin.

--- src/main/kotlin/com/anahoret/imagilabsapi/teachers/export/googlesheets/web/TeachersExportController.kt
+++ src/main/kotlin/com/anahoret/imagilabsapi/teachers/export/googlesheets/web/TeachersExportController.kt
@@ -1,18 +1,23 @@
 package com.anahoret.imagilabsapi.teachers.export.googlesheets.web
 
+import com.anahoret.imagilabsapi.applicationproperties.domain.ApplicationPropertiesKey
+import com.anahoret.imagilabsapi.applicationproperties.domain.ApplicationPropertiesService
 import com.anahoret.imagilabsapi.common.web.EmptySuccessResponseDto
 import com.anahoret.imagilabsapi.common.web.ResponseDto
+import com.anahoret.imagilabsapi.common.web.SuccessResponseDto
 import com.anahoret.imagilabsapi.security.UserRole
 import com.anahoret.imagilabsapi.teachers.export.googlesheets.config.OnGoogleSheetsIntegrationEnabled
 import com.anahoret.imagilabsapi.teachers.export.googlesheets.domain.GoogleSheetsTeachersExportUseCase
 import org.springframework.security.access.annotation.Secured
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @OnGoogleSheetsIntegrationEnabled
 class TeachersExportController(
-    private val googleSheetsTeachersExportUseCase: GoogleSheetsTeachersExportUseCase
+    private val googleSheetsTeachersExportUseCase: GoogleSheetsTeachersExportUseCase,
+    private val applicationPropertiesService: ApplicationPropertiesService
 ) {
 
     @Secured(UserRole.admin)
@@ -20,6 +25,14 @@ class TeachersExportController(
     fun exportGoogleSheetAll(): ResponseDto<Void> {
         googleSheetsTeachersExportUseCase.export()
         return EmptySuccessResponseDto
+    }
+
+    @Secured(UserRole.admin)
+    @GetMapping("/api/teachers/export/google-sheet")
+    fun getGoogleSheetUri(): ResponseDto<String?> {
+        return SuccessResponseDto(
+            applicationPropertiesService.getPropertyOrNull(ApplicationPropertiesKey.TEACHERS_EXPORT_GOOGLE_SHEET_URI)
+        )
     }
 
 }
