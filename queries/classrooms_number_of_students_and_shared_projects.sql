2c3f7789772c27021337a1551eddd56ca5a2279b
bionascu <beatrice.nsc@gmail.com>
2022-10-13 18:07:52 +0200

    let's be consistent and stick to upper case

--- queries/classrooms_number_of_students_and_shared_projects.sql
+++ queries/classrooms_number_of_students_and_shared_projects.sql
@@ -1,14 +1,14 @@
-SELECT 
-    cr.id as class_id,
-    cr.created_at as creation_date,
-    cr.name as class_name, 
-    cr.teacher_id as teacher_id,
-    COUNT(DISTINCT sp.id) as number_of_students,
-    COUNT(DISTINCT pcs.project_id) as number_of_shared_projects
-from classrooms as cr
-LEFT JOIN student_profiles as sp
-ON sp.classroom_id = cr.id
-LEFT JOIN project_classroom_share as pcs
-ON pcs.classroom_id = cr.id
-GROUP BY cr.id, cr.name, cr.teacher_id, cr.created_at
-ORDER BY creation_date asc;
+SELECT cr.id AS class_id,
+    cr.created_at AS creation_date,
+    cr.name AS class_name,
+    cr.teacher_id AS teacher_id,
+    COUNT(DISTINCT sp.id) AS number_of_students,
+    COUNT(DISTINCT pcs.project_id) AS number_of_shared_projects
+FROM classrooms AS cr
+    LEFT JOIN student_profiles AS sp ON sp.classroom_id = cr.id
+    LEFT JOIN project_classroom_share AS pcs ON pcs.classroom_id = cr.id
+GROUP BY cr.id,
+    cr.name,
+    cr.teacher_id,
+    cr.created_at
+ORDER BY creation_date ASC
