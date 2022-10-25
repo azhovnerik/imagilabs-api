e1a7898c9ac27cd9d160fd1012ecdf6a14ee4727
Muhammad Shoaib Nomani <shoaibnomani@outlook.com>
2022-10-25 18:38:05 +0500

    Add teacher profile entity repository test

---
+++ src/test/kotlin/com/anahoret/imagilabsapi/teachers/storage/TeacherProfileEntityRepositoryTest.kt
@@ -0,0 +1,45 @@
+package com.anahoret.imagilabsapi.teachers.storage
+
+import com.anahoret.imagilabsapi.spring.ImagiLabsDatabaseTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
+import java.util.UUID
+
+@ImagiLabsDatabaseTest
+@DisplayName("Teacher Profile entity repository")
+class TeacherProfileEntityRepositoryTest {
+
+    @Autowired
+    lateinit var teacherProfileEntityRepository: TeacherProfileEntityRepository
+
+    @DisplayName("when fetching teacher profile")
+    @Nested
+    inner class TeacherProfileFetchTest {
+
+        private lateinit var teacherProfileEntity: TeacherProfileEntity
+        private lateinit var teacherId: UUID
+
+        @BeforeEach
+        fun setup() {
+            setupTeacher()
+        }
+
+        private fun setupTeacher() {
+            teacherProfileEntity = teacherProfileEntityRepository.save(
+                TeacherProfileEntity("teacher@mail.com", "", "Teacher", "Edu", "Sweden", "imagi", "other", true)
+            )
+            teacherId = teacherProfileEntity.id!!
+        }
+
+        @Test
+        fun `should find teacher by id`() {
+            val result = teacherProfileEntityRepository.findByIdOrNull(teacherId)
+            assertEquals(teacherProfileEntity, result)
+        }
+    }
+}
