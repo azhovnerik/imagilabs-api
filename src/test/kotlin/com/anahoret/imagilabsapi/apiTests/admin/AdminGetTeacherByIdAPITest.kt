858ea9a81ec6a7d47de883482a55bc0ba6b04f78
Muhammad Shoaib Nomani <shoaibnomani@outlook.com>
2022-10-27 14:31:57 +0500

    Add api test for admin get teacher by id api

---
+++ src/test/kotlin/com/anahoret/imagilabsapi/apiTests/admin/AdminGetTeacherByIdAPITest.kt
@@ -0,0 +1,39 @@
+package com.anahoret.imagilabsapi.apiTests.admin
+
+import com.anahoret.imagilabsapi.auth.web.RequestAuthenticatorService
+import com.anahoret.imagilabsapi.teachers.domain.TeacherProfileService
+import com.anahoret.imagilabsapi.teachers.web.TeacherProfileController
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@DisplayName("When admin tries to fetch teacher by id")
+class AdminGetTeacherByIdAPITest (
+    @Autowired private val mockMvc: MockMvc
+) {
+
+    @MockBean
+    lateinit var requestAuthenticatorService: RequestAuthenticatorService
+
+    @MockBean
+    lateinit var teacherProfileService: TeacherProfileService
+
+    @MockBean
+    lateinit var teacherProfileController: TeacherProfileController
+
+    @Test
+    fun `should return success`() {
+        mockMvc.perform(get("/api/teachers")
+            .with(user("admin@gmail.com").password("password1").roles("ADMIN"))
+        ).andExpect(MockMvcResultMatchers.status().isOk)
+    }
+}
