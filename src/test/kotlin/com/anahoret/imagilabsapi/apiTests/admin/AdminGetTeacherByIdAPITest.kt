cd352bce2757f17bc28593accad1efec153808ad
Alexander Mikhalchenko <alexandr941989@gmail.com>
2023-05-18 12:46:38 +0300

    Refactor test to not use full Spring application to reduce test run time.

--- src/test/kotlin/com/anahoret/imagilabsapi/apiTests/admin/AdminGetTeacherByIdAPITest.kt
+++ src/test/kotlin/com/anahoret/imagilabsapi/apiTests/admin/AdminGetTeacherByIdAPITest.kt
@@ -1,33 +1,93 @@
 package com.anahoret.imagilabsapi.apiTests.admin
 
+import arrow.core.right
+import com.anahoret.imagilabsapi.admins.domain.AdminProfile
+import com.anahoret.imagilabsapi.admins.domain.AdminProfileService
+import com.anahoret.imagilabsapi.auth.web.jwt.JwtConfig
+import com.anahoret.imagilabsapi.auth.web.jwt.JwtTokenUtil
+import com.anahoret.imagilabsapi.security.AuthorityService
+import com.anahoret.imagilabsapi.security.SecurityConfig
+import com.anahoret.imagilabsapi.security.UserRole
+import com.anahoret.imagilabsapi.spring.ImagiLabsTestPropertySource
+import com.anahoret.imagilabsapi.students.domain.StudentProfileService
+import com.anahoret.imagilabsapi.teachers.domain.TeacherDeleteUseCase
+import com.anahoret.imagilabsapi.teachers.domain.TeacherGetUseCase
+import com.anahoret.imagilabsapi.teachers.domain.TeacherProfileAdminView
+import com.anahoret.imagilabsapi.teachers.domain.TeacherProfileService
 import com.anahoret.imagilabsapi.teachers.web.TeacherProfileController
+import com.anahoret.imagilabsapi.users.UserType
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.mockk
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.slf4j.Logger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
-import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
-import java.util.UUID
+import java.util.*
 
-@SpringBootTest
+@ExtendWith(SpringExtension::class)
+@WebMvcTest(
+    TeacherProfileController::class,
+    PasswordEncoder::class,
+    AuthenticationEntryPoint::class,
+    SecurityConfig::class,
+    JwtTokenUtil::class,
+    JwtConfig::class
+)
 @AutoConfigureMockMvc
 @DisplayName("When admin tries to fetch teacher by id")
-class AdminGetTeacherByIdAPITest (
-    @Autowired private val mockMvc: MockMvc
-) {
+@ImagiLabsTestPropertySource
+class AdminGetTeacherByIdAPITest {
 
-    @MockBean
-    lateinit var teacherProfileController: TeacherProfileController
+    @Autowired
+    lateinit var jwtTokenUtil: JwtTokenUtil
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @MockkBean
+    lateinit var teacherProfileService: TeacherProfileService
+
+    @MockkBean
+    lateinit var studentProfileService: StudentProfileService
+
+    @MockkBean
+    lateinit var adminProfileService: AdminProfileService
+
+    @MockkBean
+    lateinit var authorityService: AuthorityService
+
+    @MockkBean
+    lateinit var logger: Logger
+
+    @MockkBean
+    lateinit var teacherGetUseCase: TeacherGetUseCase
+
+    @MockkBean
+    lateinit var teacherDeleteUseCase: TeacherDeleteUseCase
 
     @Test
     fun `should return success`() {
         val teacherId = UUID.randomUUID()
-        mockMvc.perform(get("/api/teachers/$teacherId")
-            .with(user("admin@gmail.com").password("password1").roles("ADMIN"))
+        val adminId = UUID.randomUUID()
+        every { teacherGetUseCase.get(teacherId) } returns mockk<TeacherProfileAdminView>(relaxed = true).right()
+        val adminMock = AdminProfile(adminId, "Admin")
+        every { adminProfileService.getAdminById(adminId) } returns adminMock
+        every { authorityService.getAuthorities(adminMock) } returns listOf(SimpleGrantedAuthority(UserRole.admin))
+        val token = jwtTokenUtil.createToken(adminId, UserType.ADMIN)
+        mockMvc.perform(
+            get("/api/teachers/$teacherId")
+                .header("Authorization", "Bearer+${token.token}")
         ).andExpect(MockMvcResultMatchers.status().isOk)
     }
 }
