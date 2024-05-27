b7970f8a4c6a8577c9596ed5c22e56e0c0461112
Tatyana <taa@anadeainc.com>
2024-05-27 11:05:35 +0300

    clean up code

--- src/main/kotlin/com/anahoret/imagilabsapi/students/storage/StudentProfileEntityRepository.kt
+++ src/main/kotlin/com/anahoret/imagilabsapi/students/storage/StudentProfileEntityRepository.kt
@@ -1,7 +1,6 @@
 package com.anahoret.imagilabsapi.students.storage
 
 import org.springframework.data.domain.Sort
-import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
 import java.util.*
