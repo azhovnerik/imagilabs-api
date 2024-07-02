d2352dc705ad75dc48bffb2870972c7916d9d6cb
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-07-02 18:15:48 +0300

    Fix withdraw tip token use case for teachers.

--- src/main/kotlin/com/anahoret/imagilabsapi/openai/domain/usecases/SpendTipTokensUseCase.kt
+++ src/main/kotlin/com/anahoret/imagilabsapi/openai/domain/usecases/SpendTipTokensUseCase.kt
@@ -7,7 +7,10 @@ import com.anahoret.imagilabsapi.common.domain.error.OperationError
 import com.anahoret.imagilabsapi.common.domain.profiles.UserProfile
 import com.anahoret.imagilabsapi.openai.domain.TipTokensResponse
 import com.anahoret.imagilabsapi.openai.domain.TipTokensService
+import com.anahoret.imagilabsapi.students.domain.StudentProfile
 import com.anahoret.imagilabsapi.students.domain.StudentProfileService
+import com.anahoret.imagilabsapi.teachers.domain.TeacherProfile
+import com.anahoret.imagilabsapi.teachers.domain.TeacherProfileService
 import org.springframework.stereotype.Service
 
 interface SpendTipTokensUseCase {
@@ -17,12 +20,22 @@ interface SpendTipTokensUseCase {
 @Service
 class SpendTipTokensUseCaseImpl(
     private val studentProfileService: StudentProfileService,
+    private val teacherProfileService: TeacherProfileService,
     private val tipTokensService: TipTokensService,
     private val getTipTokensUseCase: GetTipTokensUseCase
 ) : SpendTipTokensUseCase {
 
     override fun spend(userProfile: UserProfile): Either<OperationError, TipTokensResponse> {
-        studentProfileService.getStudentById(userProfile.id) ?: return NotFoundError("STUDENT_NOT_FOUND").left()
+        when (userProfile) {
+            is StudentProfile -> studentProfileService.getStudentById(userProfile.id)
+                ?: return NotFoundError("STUDENT_NOT_FOUND").left()
+
+            is TeacherProfile -> teacherProfileService.getTeacherById(userProfile.id)
+                ?: return NotFoundError("TEACHER_NOT_FOUND").left()
+
+            else -> return NotFoundError("USER_NOT_FOUND").left()
+        }
+
         tipTokensService.withdrawOneTipToken(userProfile)
         return getTipTokensUseCase.get(userProfile)
     }
--- src/test/kotlin/com/anahoret/imagilabsapi/openai/domain/usecases/SpendTipTokensUseCaseTest.kt
+++ src/test/kotlin/com/anahoret/imagilabsapi/openai/domain/usecases/SpendTipTokensUseCaseTest.kt
@@ -1,11 +1,14 @@
 package com.anahoret.imagilabsapi.openai.domain.usecases
 
 import arrow.core.right
+import com.anahoret.imagilabsapi.admins.domain.AdminProfile
 import com.anahoret.imagilabsapi.common.domain.error.NotFoundError
-import com.anahoret.imagilabsapi.common.domain.profiles.UserProfile
 import com.anahoret.imagilabsapi.openai.domain.TipTokensResponse
 import com.anahoret.imagilabsapi.openai.domain.TipTokensService
+import com.anahoret.imagilabsapi.students.domain.StudentProfile
 import com.anahoret.imagilabsapi.students.domain.StudentProfileService
+import com.anahoret.imagilabsapi.teachers.domain.TeacherProfile
+import com.anahoret.imagilabsapi.teachers.domain.TeacherProfileService
 import com.anahoret.imagilabsapi.users.UserType
 import io.mockk.every
 import io.mockk.justRun
@@ -20,20 +23,32 @@ class SpendTipTokensUseCaseTest {
 
     private val tipTokensService = mockk<TipTokensService>()
     private val studentProfileService = mockk<StudentProfileService>()
+    private val teacherProfileService = mockk<TeacherProfileService>()
     private val getTipTokensUseCase = mockk<GetTipTokensUseCase>()
     private val spendTipTokensUseCase =
-        SpendTipTokensUseCaseImpl(studentProfileService, tipTokensService, getTipTokensUseCase)
+        SpendTipTokensUseCaseImpl(studentProfileService, teacherProfileService, tipTokensService, getTipTokensUseCase)
 
     private val studentId = UUID.randomUUID()
-    private val userProfile = mockk<UserProfile> {
+    private val teacherId = UUID.randomUUID()
+    private val studentProfile = mockk<StudentProfile> {
         every { id } returns studentId
+        every { userType } returns UserType.STUDENT
+    }
+
+    private val teacherProfile = mockk<TeacherProfile> {
+        every { id } returns teacherId
+        every { userType } returns UserType.STUDENT
+    }
+
+    private val adminProfile = mockk<AdminProfile> {
+        every { id } returns teacherId
         every { userType } returns UserType.STUDENT
     }
 
     @Test
     fun `should return error when student not found`() {
         every { studentProfileService.getStudentById(studentId) } returns null
-        spendTipTokensUseCase.spend(userProfile).fold(
+        spendTipTokensUseCase.spend(studentProfile).fold(
             {
                 assertAll(
                     { assertTrue(it is NotFoundError) },
@@ -44,11 +59,47 @@ class SpendTipTokensUseCaseTest {
     }
 
     @Test
-    fun `should spend tip token data`() {
+    fun `should return error when teacher not found`() {
+        every { teacherProfileService.getTeacherById(teacherId) } returns null
+        spendTipTokensUseCase.spend(teacherProfile).fold(
+            {
+                assertAll(
+                    { assertTrue(it is NotFoundError) },
+                    { assertEquals("TEACHER_NOT_FOUND", (it as NotFoundError).message) }
+                )
+            }, { fail() }
+        )
+    }
+
+    @Test
+    fun `should return error when user is not teacher nor student`() {
+        val userId = UUID.randomUUID()
+        every { studentProfileService.getStudentById(userId) } returns null
+        every { teacherProfileService.getTeacherById(userId) } returns null
+        spendTipTokensUseCase.spend(adminProfile).fold(
+            {
+                assertAll(
+                    { assertTrue(it is NotFoundError) },
+                    { assertEquals("USER_NOT_FOUND", (it as NotFoundError).message) }
+                )
+            }, { fail() }
+        )
+    }
+
+    @Test
+    fun `should spend tip token data for student`() {
         every { studentProfileService.getStudentById(studentId) } returns mockk()
-        justRun { tipTokensService.withdrawOneTipToken(userProfile) }
-        every { getTipTokensUseCase.get(userProfile) } returns TipTokensResponse(3, 60L).right()
-        spendTipTokensUseCase.spend(userProfile).fold({ fail() }, { assertEquals(3, it.leftTipTokens) })
+        justRun { tipTokensService.withdrawOneTipToken(studentProfile) }
+        every { getTipTokensUseCase.get(studentProfile) } returns TipTokensResponse(3, 60L).right()
+        spendTipTokensUseCase.spend(studentProfile).fold({ fail() }, { assertEquals(3, it.leftTipTokens) })
+    }
+
+    @Test
+    fun `should spend tip token data for teacher`() {
+        every { teacherProfileService.getTeacherById(teacherId) } returns mockk()
+        justRun { tipTokensService.withdrawOneTipToken(teacherProfile) }
+        every { getTipTokensUseCase.get(teacherProfile) } returns TipTokensResponse(3, 60L).right()
+        spendTipTokensUseCase.spend(teacherProfile).fold({ fail() }, { assertEquals(3, it.leftTipTokens) })
     }
 
 }
