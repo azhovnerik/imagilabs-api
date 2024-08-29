d0b0cf7af3008d8add87f170827be6d80eae1c50
Alexander Mikhalchenko <alexandr941989@gmail.com>
2024-08-29 15:19:28 +0300

    Allow student access to OpenAI controller.

--- src/main/kotlin/com/anahoret/imagilabsapi/openai/web/OpenAiController.kt
+++ src/main/kotlin/com/anahoret/imagilabsapi/openai/web/OpenAiController.kt
@@ -17,7 +17,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
-@Secured(UserRole.teacher)
+@Secured(UserRole.teacher, UserRole.student)
 @RestController
 class OpenAiController(
     private val getOpenAiResponseOnSuccess: GetOpenAiAssistanceOnSuccessUseCase,
