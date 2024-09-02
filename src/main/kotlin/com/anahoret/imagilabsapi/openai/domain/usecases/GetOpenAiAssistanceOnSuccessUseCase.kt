31210dd78437ca5d63238131a8a19490c6758dd5
Alexander <alexandr941989@gmail.com>
2024-09-02 17:42:03 +0300

    Remove debug print.

--- src/main/kotlin/com/anahoret/imagilabsapi/openai/domain/usecases/GetOpenAiAssistanceOnSuccessUseCase.kt
+++ src/main/kotlin/com/anahoret/imagilabsapi/openai/domain/usecases/GetOpenAiAssistanceOnSuccessUseCase.kt
@@ -37,7 +37,6 @@ class GetOpenAiAssistanceOnSuccessUseCaseImpl(
         userProfile: UserProfile
     ): AssistanceResponse {
         val userQuestion = OpenAiPrompts.questionDirective(request.userCode, request.userQuestion)
-        println("#$userQuestion#")
         val aiResponse = openAiService.startAssistance(userQuestion)
         val openAiAssistance = openAiAssistanceService.save(userProfile, userQuestion, aiResponse, request)
         tipTokensService.withdrawOneTipToken(userProfile)
