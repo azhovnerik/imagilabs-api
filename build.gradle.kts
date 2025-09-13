4b52074161a8dd3fabc02ccb69851e6ddb088e9a
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-09-13 16:37:44 +0300

    Exclude commons-logging package to fix dependency conflict.

--- build.gradle.kts
+++ build.gradle.kts
@@ -59,7 +59,9 @@ dependencies {
 
     // Utils
     implementation("io.arrow-kt:arrow-core:$arrowKtVersion")
-    implementation("org.apache.pdfbox:pdfbox:$pdfBoxVersion")
+    implementation("org.apache.pdfbox:pdfbox:$pdfBoxVersion") {
+        exclude(group = "commons-logging", module = "commons-logging")
+    }
     implementation("org.apache.commons:commons-lang3:$commonsLangVersion")
 
     // Web
@@ -82,7 +84,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-mail")
 
     // Google
-    implementation("com.google.apis:google-api-services-sheets:$googleSheetsApiVersion")
+    implementation("com.google.apis:google-api-services-sheets:$googleSheetsApiVersion") {
+        exclude(group = "commons-logging", module = "commons-logging")
+    }
     implementation("com.google.auth:google-auth-library-oauth2-http:$googleAuthVersion")
 
     // ShedLock
