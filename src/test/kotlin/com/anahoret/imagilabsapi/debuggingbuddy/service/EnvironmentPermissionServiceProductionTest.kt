62e72e787bc69abb33461926fc3b48c97f39f79d
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-10-23 18:01:34 +0300

    Update tests to cover Hour of AI event dates.

--- src/test/kotlin/com/anahoret/imagilabsapi/debuggingbuddy/service/EnvironmentPermissionServiceProductionTest.kt
+++ src/test/kotlin/com/anahoret/imagilabsapi/debuggingbuddy/service/EnvironmentPermissionServiceProductionTest.kt
@@ -54,26 +54,33 @@ class EnvironmentPermissionServiceProductionTest {
     }
 
     @Test
-    fun `should return true when user is student and date is before October 18 2024`() {
-        val september162024 = ZonedDateTime.of(2024, 9, 16, 0, 0, 0, 0, ZoneId.of("UTC-7"))
-        every { clock.millis() } returns september162024.toInstant().toEpochMilli()
+    fun `should return true when date is between November 8 and December 15 of 2025`() {
+        val november82025 = ZonedDateTime.of(2025, 11, 8, 0, 0, 0, 0, ZoneId.of("UTC-8"))
+        every { clock.millis() } returns november82025.toInstant().toEpochMilli()
         assertTrue(environmentPermissionServiceProduction.canGetAssistanceForProject(mockk<StudentProfile>()))
     }
 
     @Test
-    fun `should return true when user is student and date is October 18 2024`() {
-        val october182024 = ZonedDateTime.of(LocalDate.of(2024, 10, 18), LocalTime.MAX, ZoneId.of("UTC-7"))
-        every { clock.millis() } returns october182024.toInstant().toEpochMilli()
-        assertTrue(environmentPermissionServiceProduction.canGetAssistanceForProject(mockk<StudentProfile>()))
-    }
-
-    @Test
-    fun `should return false when user is student and their teacher has no subscription and date is after October 18 2024`() {
-        val october192024 =
-            ZonedDateTime.of(LocalDate.of(2024, 10, 19), LocalTime.MIN, ZoneId.of("UTC-7")).toInstant().toEpochMilli()
-        every { clock.millis() } returns october192024
+    fun `should return false when user is student and their teacher has no subscription and date is after December 15 of 2025`() {
+        val december162025 =
+            ZonedDateTime.of(LocalDate.of(2025, 12, 16), LocalTime.MIN, ZoneId.of("UTC-8")).toInstant().toEpochMilli()
+        every { clock.millis() } returns december162025
         every { teacherProfileService.getTeacherByStudent(studentId) } returns mockk<TeacherProfile> {
-            every { hasProSubscription(october192024) } returns false
+            every { hasProSubscription(december162025) } returns false
+        }
+        val studentProfile = mockk<StudentProfile> {
+            every { id } returns studentId
+        }
+        assertFalse(environmentPermissionServiceProduction.canGetAssistanceForProject(studentProfile))
+    }
+
+    @Test
+    fun `should return false when user is student and their teacher has no subscription and date is before November 8 of 2025`() {
+        val november72025 =
+            ZonedDateTime.of(LocalDate.of(2025, 11, 7), LocalTime.MAX, ZoneId.of("UTC-8")).toInstant().toEpochMilli()
+        every { clock.millis() } returns november72025
+        every { teacherProfileService.getTeacherByStudent(studentId) } returns mockk<TeacherProfile> {
+            every { hasProSubscription(november72025) } returns false
         }
         val studentProfile = mockk<StudentProfile> {
             every { id } returns studentId
