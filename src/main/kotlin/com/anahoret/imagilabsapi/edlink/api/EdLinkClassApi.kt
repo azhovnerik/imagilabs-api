67921f6f3e015d7141e6b2a295a0c99348a0e9a8
Alexander Mikhalchenko <alexandr941989@gmail.com>
2025-11-04 12:05:32 +0200

    Add EdLink class API.

---
+++ src/main/kotlin/com/anahoret/imagilabsapi/edlink/api/EdLinkClassApi.kt
@@ -0,0 +1,71 @@
+package com.anahoret.imagilabsapi.edlink.api
+
+import arrow.core.Either
+import arrow.core.left
+import arrow.core.right
+import com.anahoret.imagilabsapi.common.domain.error.OperationError
+import com.anahoret.imagilabsapi.common.domain.security.AccessDeniedError
+import com.anahoret.imagilabsapi.edlink.api.model.EdLinkClass
+import com.anahoret.imagilabsapi.edlink.api.model.EdLinkResponseList
+import com.anahoret.imagilabsapi.edlink.api.model.Person
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpHeaders
+import org.springframework.http.RequestEntity
+import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import java.util.*
+
+interface EdLinkClassApi {
+    fun listClasses(token: String): Either<OperationError, List<EdLinkClass>>
+    fun listTeachers(token: String, classId: UUID): Either<OperationError, List<Person>>
+    fun listStudents(token: String, classId: UUID): Either<OperationError, List<Person>>
+}
+
+@Service
+class EdLinkClassApiImpl(
+    private val edLinkRestTemplate: RestTemplate
+) : EdLinkClassApi {
+
+    override fun listClasses(token: String): Either<OperationError, List<EdLinkClass>> {
+        val request = RequestEntity<Void>
+            .get("/v2/graph/classes")
+            .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+            .build()
+        return edLinkRestTemplate
+            .exchange(request, object : ParameterizedTypeReference<EdLinkResponseList<EdLinkClass>>() {})
+            .takeIf { it.statusCode.is2xxSuccessful }
+            ?.body
+            ?.data
+            ?.right()
+            ?: AccessDeniedError("ED_LINK_API_FAILED_TO_LIST_CLASSES").left()
+    }
+
+    override fun listTeachers(token: String, classId: UUID): Either<OperationError, List<Person>> {
+        val request = RequestEntity<Void>
+            .get("/v2/graph/classes/$classId/teachers")
+            .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+            .build()
+        return edLinkRestTemplate
+            .exchange(request, object : ParameterizedTypeReference<EdLinkResponseList<Person>>() {})
+            .takeIf { it.statusCode.is2xxSuccessful }
+            ?.body
+            ?.data
+            ?.right()
+            ?: AccessDeniedError("ED_LINK_API_FAILED_TO_LIST_TEACHERS_FOR_CLASS").left()
+    }
+
+    override fun listStudents(token: String, classId: UUID): Either<OperationError, List<Person>> {
+        val request = RequestEntity<Void>
+            .get("/v2/graph/classes/$classId/students")
+            .header(HttpHeaders.AUTHORIZATION, "Bearer $token")
+            .build()
+        return edLinkRestTemplate
+            .exchange(request, object : ParameterizedTypeReference<EdLinkResponseList<Person>>() {})
+            .takeIf { it.statusCode.is2xxSuccessful }
+            ?.body
+            ?.data
+            ?.right()
+            ?: AccessDeniedError("ED_LINK_API_FAILED_TO_LIST_STUDENTS_FOR_CLASS").left()
+    }
+
+}
