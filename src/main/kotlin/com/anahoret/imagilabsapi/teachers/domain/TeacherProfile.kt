ff0b6e3b50f7a0e9da9def3d9940e00e2c6b6428
andrejandrej <azh@anadeainc.com>
2025-12-02 15:37:03 +0200

    add howDidYouHearAboutUs, howDidYouHearAboutUsOther

--- src/main/kotlin/com/anahoret/imagilabsapi/teachers/domain/TeacherProfile.kt
+++ src/main/kotlin/com/anahoret/imagilabsapi/teachers/domain/TeacherProfile.kt
@@ -7,6 +7,7 @@ import com.anahoret.imagilabsapi.teachers.storage.TeacherProfileEntity
 import com.anahoret.imagilabsapi.users.UserType
 import com.fasterxml.jackson.annotation.JsonIgnore
 import java.util.*
+import kotlin.String
 
 @Suppress("unused")
 class TeacherProfile(
@@ -16,6 +17,8 @@ class TeacherProfile(
     var email: String,
     val country: String,
     val organization: String,
+    val howDidYouHearAboutUs: String,
+    val howDidYouHearAboutUsOther: String?,
     val createdAt: Long,
     val emailVerified: Boolean,
     val marketingEmailSubscribed: Boolean,
@@ -26,12 +29,14 @@ class TeacherProfile(
     val subjects: String? = null,
     val schools: String? = null,
     @field:JsonIgnore val edLinkIntegrationId: UUID? = null,
-    @field:JsonIgnore val edLinkPersonId: UUID? = null
-) : UserProfile, TeacherSubscriptionData by subscription {
+    @field:JsonIgnore val edLinkPersonId: UUID? = null,
+) : UserProfile {
 
     override val userType = UserType.TEACHER
     override val fullName = "$firstName $lastName"
     val isEdLinkConnected = edLinkIntegrationId != null && edLinkPersonId != null
+
+    fun hasProSubscription(now: Long): Boolean = subscription.hasProSubscription(now)
 
     companion object {
 
@@ -44,6 +49,8 @@ class TeacherProfile(
                     email,
                     country,
                     organization,
+                    howDidYouHearAboutUs,
+                    howDidYouHearAboutUsOther,
                     createdAt ?: 0,
                     emailVerified,
                     marketingEmailSubscribed,
